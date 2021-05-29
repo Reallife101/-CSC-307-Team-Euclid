3b20e9c45a403500b0fa55f52afd922ad51b1b66
Ryan Ngoon <ryan.ngoon@gmail.com>
2021-05-29 15:23:40 -0700

    Fix firebase

--- pages/database.js
+++ pages/database.js
@@ -1,10 +1,3 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const { request } = require("express");
-const firebase = require("firebase/app") //https://www.gstatic.com/firebasejs/8.6.1/firebase-app.js"
-require("firebase/analytics")            //"https://www.gstatic.com/firebasejs/8.6.1/firebase-analytics.js"
-require("firebase/database")             //"https://www.gstatic.com/firebasejs/8.6.1/firebase-database.js"
-
 var firebaseConfig = {
     apiKey:        "AIzaSyDl0gkgD1D9usTr8ngnMr5bRVu5Ac2jbmg",
     authDomain:    "euclidean-geometry-61a34.firebaseapp.com",
